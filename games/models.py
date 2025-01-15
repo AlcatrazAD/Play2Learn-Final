1f86443d5f3075fef3b0c62ca4ac16e8a0520371
AlcatrazAD <Reach.liadavis@gmail.com>
2025-01-14 18:19:45 -0600

    added options for gamescore operations

--- games/models.py
+++ games/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class GameScore(models.Model):
+
+    MATH = "MATH"
+    ANAGRAM = "ANAGRAM" 
+
+    GAME_CHOICES = [
+        (MATH, "Math Facts"),
+        (ANAGRAM, "Anagram Hunt")
+    ]
+
+    MULTIPLICATION = 'x'
+    DIVISION = '/'
+    SUBTRACTION = '-'
+    ADDITION = '+'
+    NONAPPLICABLE = 'n/a'
+
+    OPERATIONS = [
+     (MULTIPLICATION, 'x'),
+     (DIVISION, '/'),
+     (SUBTRACTION,'-'),
+     (ADDITION,'+'),
+     (NONAPPLICABLE,'N/A'),
+    
+    ]
+
+    user_name = models.TextField()
+    game = models.TextField(choices=GAME_CHOICES, default=MATH)
+    operation = models.TextField(choices=OPERATIONS, default=ADDITION)
+    max_number = models.IntegerField()
+    score = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
