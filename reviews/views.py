2f8b74af897703963520e9f2a218a5eef2f7aa70
AlcatrazAD <Reach.liadavis@gmail.com>
2025-01-17 20:41:26 -0600

    reviews prt 1

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Review
+
+class ReviewListView(ListView):
+    model = Review
 
 # Create your views here.
