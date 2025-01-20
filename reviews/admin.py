38dd1d189ed207bbba1acdfbfeae91320f5ef881
AlcatrazAD <Reach.liadavis@gmail.com>
2025-01-20 11:50:23 -0600

    admin up 2

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ['submission', 'created']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('created')
+
+        return ()
