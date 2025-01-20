49bfb7eab35fd00b51593fec2e1c861773fa9900
AlcatrazAD <Reach.liadavis@gmail.com>
2025-01-19 19:54:24 -0600

    reviews have been revieweded

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,24 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import  CreateView, DetailView, ListView, UpdateView
 
 from .models import Review
+
+class ReviewCreateView(CreateView):
+    model = Review
+    fields = ['submission']
+
+
 
 class ReviewDetailView(DetailView):
     model = Review
 
 class ReviewListView(ListView):
     model = Review
+
+
+class ReviewUpdateView(UpdateView):
+    model = Review
+    fields = ['submission']
+
 
 
 # Create your views here.
