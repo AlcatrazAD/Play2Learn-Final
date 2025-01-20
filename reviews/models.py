4d65896607223a991bc4cc4c6d52e74bf42bfe18
AlcatrazAD <Reach.liadavis@gmail.com>
2025-01-19 19:02:01 -0600

    reviews base finish

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 
 class Review(models.Model):
     submission = models.TextField(max_length=200)
     
     created = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('reviews:detail', args=[str(self.pk)])
    
     def __str__(self):
         return self.submission
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Review
 
+class ReviewDetailView(DetailView):
+    model = Review
+
 class ReviewListView(ListView):
     model = Review
+
 
 # Create your views here.
